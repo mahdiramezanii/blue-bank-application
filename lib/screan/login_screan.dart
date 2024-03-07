3f065f55701a6a048a31fa14c0bb083ffe792a22
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-03-08 03:26:59 +0330

    initial

---
+++ lib/screan/login_screan.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+
+class LoginScrean extends StatelessWidget{
+
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      body: Center(child: Text("Mahdi ramazani"),),
+    );
+  }
+}
