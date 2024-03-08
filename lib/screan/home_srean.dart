54aefa5bfa4034c12d25dbf40f5e130027b9c599
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-03-08 17:35:54 +0330

    complated header home_page

--- lib/screan/home_srean.dart
+++ lib/screan/home_srean.dart
@@ -1,14 +1,56 @@
 import "package:flutter/material.dart";
 
-
-class HomeScrean extends StatelessWidget{
-
+class HomeScrean extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-
-
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center( child: Text("Home"),),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.blue,
+            automaticallyImplyLeading: false,
+            expandedHeight: 170,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Column(
+                children: [
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.help,
+                        color: Colors.white,
+                      ),
+                      Icon(
+                        Icons.notifications_none_outlined,
+                        color: Colors.white,
+                      ),
+                      Spacer(),
+                      Text(
+                        "خانه",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900),
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.notifications_none_outlined,
+                        color: Colors.white,
+                      ),
+                      Icon(
+                        Icons.notifications_none_outlined,
+                        color: Colors.white,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
