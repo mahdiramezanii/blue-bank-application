a0622b4e77d908abef0c37b4b9798ec04139cde5
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-03-11 00:44:27 +0330

    comaplted activiti screan

--- lib/screan/activity_screan.dart
+++ lib/screan/activity_screan.dart
@@ -79,19 +79,150 @@ class ActivityScrean extends StatelessWidget {
             Container(
               width: 370,
               height: 100,
-             decoration: BoxDecoration(
-               color: Colors.blue,
-              borderRadius: BorderRadiusDirectional.circular(20)
-             ),
-            ),
-            Container(
-              height: 60,
-              width: 60,
               decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(15)
+                  color: Colors.blue,
+                  borderRadius: BorderRadiusDirectional.circular(20)),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        height: 65,
+                        width: 65,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(15)),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "برگشت پول",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w800),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: 65,
+                        width: 65,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(15)),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "قبض",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w800),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: 65,
+                        width: 65,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(15)),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "اینترنت",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w800),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: 65,
+                        width: 65,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(15)),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "شارژ",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w800),
+                      )
+                    ],
+                  ),
+                ],
               ),
-            )
+
+            ),
+            SizedBox(height: 15,),
+
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 27),
+              child: Row(
+               mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        height: 65,
+                        width: 65,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(15)),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "پرداخت خودکار",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w800),
+                      )
+                    ],
+                  ),
+                  SizedBox(width: 5,),
+                  Column(
+                    children: [
+                      Container(
+                        height: 65,
+                        width: 65,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(15)),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "وام",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w800),
+                      )
+                    ],
+                  ),
+                
+                ],
+              ),
+
+            ),
           ],
         ),
       ),
