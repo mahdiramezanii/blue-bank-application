2f5cd1c456c89f89908f65c51d558d53f47b32df
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-03-11 11:38:21 +0330

    fix bug sliver grade in activity screan

--- lib/screan/activity_screan.dart
+++ lib/screan/activity_screan.dart
@@ -5,100 +5,105 @@ class ActivityScrean extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: NestedScrollView(
-          headerSliverBuilder: ((context, innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                bottom: PreferredSize(
-                  preferredSize: Size.fromHeight(15),
-                  child: Container(
-                    height: 15,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        topRight: Radius.circular(15),
+        headerSliverBuilder: ((context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(15),
+                child: Container(
+                  height: 15,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    ),
+                  ),
+                ),
+              ),
+              expandedHeight: 315,
+              pinned: true,
+              floating: true,
+              snap: false,
+              elevation: 2.0,
+              backgroundColor: Colors.blue,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Container(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 50),
                       ),
-                    ),
-                  ),
-                ),
-                expandedHeight: 315,
-                pinned: true,
-                floating: true,
-                snap: false,
-                elevation: 2.0,
-                backgroundColor: Colors.blue,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Container(
-                    child: Column(
-                      children: [
-                        Padding(padding: EdgeInsets.only(top: 50)),
-                        Row(
-                          children: [
-                            SizedBox(
-                              width: 13,
-                            ),
-                            Icon(
-                              Icons.help,
-                              size: 30,
-                              color: Colors.white,
-                            )
-                          ],
-                        ),
-                        SizedBox(
-                          width: 170,
-                          child: Image(
-                            image: AssetImage("assets/images/qrcode.png"),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 13,
                           ),
-                        ),
-                        Text(
-                          "QR انتقال و دریافت با کد",
-                          style: TextStyle(
+                          Icon(
+                            Icons.help,
+                            size: 30,
                             color: Colors.white,
-                            fontSize: 25,
-                          ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        width: 170,
+                        child: Image(
+                          image: AssetImage("assets/images/qrcode.png"),
                         ),
-                        Icon(
-                          Icons.keyboard_double_arrow_down_outlined,
-                          size: 40,
+                      ),
+                      Text(
+                        "QR انتقال و دریافت با کد",
+                        style: TextStyle(
                           color: Colors.white,
-                        )
-                      ],
-                    ),
+                          fontSize: 25,
+                        ),
+                      ),
+                      Icon(
+                        Icons.keyboard_double_arrow_down_outlined,
+                        size: 40,
+                        color: Colors.white,
+                      )
+                    ],
                   ),
                 ),
               ),
-              SliverPersistentHeader(
-                pinned: true,
-                delegate: HeaderPressistent(),
-              )
-            ];
-          }),
-          body: Column(
-            children: [
-              Padding(padding: EdgeInsets.only(top: 30)),
-            Container(
-              width: 370,
-              height: 100,
-              decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadiusDirectional.circular(20)),
             ),
-              GridView.builder(
-                itemCount: 6,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4, crossAxisSpacing: 1, mainAxisSpacing: 1),
-                itemBuilder: (BuildContext context, index) {
-                  return Container(
-                    height: 65,
-                    width: 65,
-                    decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(15)),
-                  );
-                },
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: HeaderPressistent(),
+            )
+          ];
+        }),
+        body: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Container(
+                width: 370,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadiusDirectional.circular(20),
+                ),
               ),
-            ],
-          )),
+            ),
+            SliverGrid(
+              delegate: SliverChildBuilderDelegate((context, index) {
+                return Container(
+                  height: 65,
+                  width: 65,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                );
+              }),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3, mainAxisSpacing: 2, crossAxisSpacing: 3),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
